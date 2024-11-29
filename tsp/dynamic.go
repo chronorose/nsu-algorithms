0776580b9cc4c27dc40dac687060c172d9c7b819
rendemare <rendemare@gmail.com>
2024-11-29 17:12:25 +0700

    TSP moment.

--- tsp/dynamic.go
+++ tsp/dynamic.go
@@ -29,7 +29,6 @@ func bhk(graph [][]int) {
 		curS := S[i]
 		for j := 0; j < len(curS); j++ {
 			curV := curS[j]
-			// fmt.Print(curV, " ")
 			ans := 10001
 			for k := 1; k < length; k++ {
 				if curV&(1<<k) == 0 {
@@ -40,24 +39,14 @@ func bhk(graph [][]int) {
 					if (curV&(1<<h) == 0) || h == k {
 						continue
 					}
-					fmt.Println("SmV:", SmV, "graph:", graph[k][h], "k:", k, "h:", h, "A[h][SmV]:", A[h][SmV])
 					ans = min(A[h][SmV]+graph[k][h], ans)
 				}
 				A[k][curV] = ans
-				fmt.Println("k:", k, "curV:", curV, "ans:", ans)
-				fmt.Println()
 			}
 		}
 	}
 	ans := 100000
 	l := (1 << length) - 1
-	y := int(math.Pow(2, float64(length)))
-	for i := 0; i < length; i++ {
-		for j := 0; j < y; j++ {
-			fmt.Print(A[i][j], " ")
-		}
-		fmt.Println()
-	}
 	for i := 2; i < length; i++ {
 		ans = min(ans, A[i][l]+graph[i][0])
 	}
--- tsp/main.go
+++ tsp/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
+// "fmt"
 )
 
 func main() {
-	fmt.Println("Work in progress.")
 	graph := parseTest("kek")
-	fmt.Println(naiveTSP(graph))
+	// fmt.Println(naiveTSP(graph))
 	bhk(graph)
 }
